a194f3b5a4e758b1cc8aead0f815857edaf03183
Gokul <gokulchandan24@gmail.com>
2024-05-01 13:41:06 +0530

    added assets

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,27 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Gokul has finally done it</Text>
+    <View style={modalstyles.container}>
+      <Text style={modalstyles.font}>Milena AI</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to profile
+      </Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const modalstyles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  font: {
+    fontSize: 30,
   },
 });
